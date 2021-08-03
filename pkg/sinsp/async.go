75b5e237183110035d6b75cdf413f5620c0016f1
Mark Stemm <mark.stemm@gmail.com>
2021-08-03 13:57:57 -0500

    Add wrappers to handle async c to golang type conversions
    This avoids having to duplicate the same calls to C.GoString/C.GoBytes
    everywhere, at the expense of some unsafe.Pointer usage.

--- pkg/sinsp/async.go
+++ pkg/sinsp/async.go
@@ -19,6 +19,42 @@ import (
 	"unsafe"
 )
 
+// These helpers avoid duplicating the same conversion code in plugins
+// that want to use the async extractor functions. It circuments the
+// problems in https://github.com/golang/go/issues/13466.
+//
+// It also uses unsafe.Pointer so be careful with its use!
+
+func WrapExtractStr(plgState unsafe.Pointer, evtnum uint64, field unsafe.Pointer, arg unsafe.Pointer, data unsafe.Pointer, datalen uint32, strExtractorFunc PluginExtractStrFunc) unsafe.Pointer {
+	fieldStr := C.GoString((*C.char)(field))
+	argStr := C.GoString((*C.char)(arg))
+	dataBuf := C.GoBytes(data, C.int(datalen))
+
+	present, extractStr := strExtractorFunc(plgState, evtnum, fieldStr, argStr, dataBuf)
+
+	if (!present) {
+		return nil
+	}
+
+	return unsafe.Pointer(C.CString(extractStr))
+}
+
+func WrapExtractU64(plgState unsafe.Pointer, evtnum uint64, field unsafe.Pointer, arg unsafe.Pointer, data unsafe.Pointer, datalen uint32, fieldPresent *uint32, u64ExtractorFunc PluginExtractU64Func) uint64 {
+	fieldStr := C.GoString((*C.char)(field))
+	argStr := C.GoString((*C.char)(arg))
+	dataBuf := C.GoBytes(data, C.int(datalen))
+
+	present, extractU64 := u64ExtractorFunc(plgState, evtnum, fieldStr, argStr, dataBuf)
+
+	if (!present) {
+		*fieldPresent = 0
+		return 0
+	}
+
+	*fieldPresent = 1
+	return extractU64
+}
+
 // RegisterAsyncExtractors is a helper function to be used within plugin_register_async_extractor.
 //
 // Intended usage as in the following example:
@@ -31,6 +67,17 @@ import (
 //     // A function called by plugin_extract_u64 after conversion from C types to go types
 //     func extract_u64(pluginState unsafe.Pointer, evtnum uint64, field string, arg string, data []byte) (bool, uint64) {
 //      ...
+//     }
+//
+//     //export plugin_extract_str
+//     func plugin_extract_str(plgState unsafe.Pointer, evtnum uint64, field *C.char, arg *C.char, data *C.uint8_t, datalen uint32) *C.char {
+//      // A plugin can also do their own conversion, but this wrapper handles the conversion automatically.
+//	return (*C.char)(sinsp.WrapExtractStr(plgState, evtnum, unsafe.Pointer(field), unsafe.Pointer(arg), unsafe.Pointer(data), datalen, extract_str))
+//     }
+//
+//     //export plugin_extract_u64
+//     func plugin_extract_u64(plgState unsafe.Pointer, evtnum uint64, field *C.char, arg *C.char, data *C.uint8_t, datalen uint32, fieldPresent *uint32) uint64 {
+//	return sinsp.WrapExtractU64(plgState, evtnum, unsafe.Pointer(field), unsafe.Pointer(arg), unsafe.Pointer(data), datalen, fieldPresent, extract_u64)
 //     }
 //
 //     //export plugin_register_async_extractor
